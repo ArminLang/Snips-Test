7197c66f816984eecfb19fd738fdb50d9b3b1e9f
ArminLang <43016464+ArminLang@users.noreply.github.com>
2018-09-07 00:02:10 +0200

    Update action-hello.py

--- action-hello.py
+++ action-hello.py
@@ -5,6 +5,8 @@ import ConfigParser
 from hermes_python.hermes import Hermes
 from hermes_python.ontology import *
 import io
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
 
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI = "config.ini"
@@ -30,12 +32,18 @@ def subscribe_intent_callback(hermes, intentMessage):
 
 def action_wrapper(hermes, intentMessage, conf):
     current_session_id = intentMessage.session_id
-  #  hermes.publish("lgtv/set/toast","YES")
+    mqtt_client.publish("lgtv/set/toast","YES")
         
     hermes.publish_end_session(current_session_id, "Hello World")
 
 
 if __name__ == "__main__":
+    mqtt_client = mqtt.Client()
+ #   mqtt_client.message_callback_add('hermes/intent/#', on_message_intent)
+    mqtt_client.connect("localhost", "1883")
+#    mqtt_client.subscribe('hermes/intent/#')
+    mqtt_client.loop_forever()
+    
     with Hermes("localhost:1883") as h:
         h.subscribe_intent("arminlang:sonosInvade", subscribe_intent_callback) \
          .start()
