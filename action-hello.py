362b2b62a9bec74c2e92935f5cd5d40c4d701dc6
ArminLang <43016464+ArminLang@users.noreply.github.com>
2018-09-06 22:40:13 +0200

    Update action-hello.py

--- action-hello.py
+++ action-hello.py
@@ -35,5 +35,5 @@ def action_wrapper(hermes, intentMessage, conf):
 
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
-        h.subscribe_intent("CrystalMethod:hello", subscribe_intent_callback) \
+        h.subscribe_intent("arminlang:sonosInvade", subscribe_intent_callback) \
          .start()
